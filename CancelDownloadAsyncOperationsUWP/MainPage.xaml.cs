ac21f5716597d902e4336bf5144bba86eb6ebbb3
Lon <gavinlondee@gmail.com>
2020-10-04 17:52:06 +0200

    Initial commit

---
+++ CancelDownloadAsyncOperationsUWP/MainPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CancelDownloadAsyncOperationsUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        readonly CancellationTokenSource s_cts = new CancellationTokenSource();
+
+        readonly HttpClient s_client = new HttpClient
+        {
+            MaxResponseContentBufferSize = 1_000_000
+        };
+
+        readonly IEnumerable<string> s_urlList = new string[]
+        {
+            "https://docs.microsoft.com",
+            "https://docs.microsoft.com/aspnet/core",
+            "https://docs.microsoft.com/azure",
+            "https://docs.microsoft.com/azure/devops",
+            "https://docs.microsoft.com/dotnet",
+            "https://docs.microsoft.com/dynamics365",
+            "https://docs.microsoft.com/education",
+            "https://docs.microsoft.com/enterprise-mobility-security",
+            "https://docs.microsoft.com/gaming",
+            "https://docs.microsoft.com/graph",
+            "https://docs.microsoft.com/microsoft-365",
+            "https://docs.microsoft.com/office",
+            "https://docs.microsoft.com/powershell",
+            "https://docs.microsoft.com/sql",
+            "https://docs.microsoft.com/surface",
+            "https://docs.microsoft.com/system-center",
+            "https://docs.microsoft.com/visualstudio",
+            "https://docs.microsoft.com/windows",
+            "https://docs.microsoft.com/xamarin"
+        };
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void AddListItem(string text)
+        {
+            ListViewItem listViewItem = new ListViewItem();
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = text;
+            listViewItem.Content = textBlock;
+            lvwOutput.Items.Add(listViewItem);
+
+        }
+        private async Task SumPageSizesAsync()
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            int total = 0;
+            foreach (string url in s_urlList)
+            {
+                int contentLength = await ProcessUrlAsync(url, s_client, s_cts.Token);
+                total += contentLength;
+            }
+
+            stopwatch.Stop();
+
+            AddListItem($"Total bytes returned:  {total:#,#}");
+            AddListItem($"Elapsed time:          {stopwatch.Elapsed}");
+        }
+        private async Task<int> ProcessUrlAsync(string url, HttpClient client, CancellationToken token)
+        {
+            HttpResponseMessage response = await client.GetAsync(url, token);
+            byte[] content = await response.Content.ReadAsByteArrayAsync();
+            AddListItem($"{url,-60} {content.Length,10:#,#}");
+
+            return content.Length;
+        }
+
+        private async void btnDownloadContent_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               // s_cts.CancelAfter(3500);
+                await SumPageSizesAsync();
+            }
+            catch (TaskCanceledException)
+            {
+                AddListItem("Download Operations Cancelled");
+            }
+            
+        }
+
+        private void btnCancelDownload_Click(object sender, RoutedEventArgs e)
+        {
+            s_cts.Cancel();
+        }
+    }
+}
